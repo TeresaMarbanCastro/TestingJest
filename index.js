dbc9b4bc662c428c86a5413201eab18485c0cf5b
Teresa <teresamarbancastro@gmail.com>
2018-05-14 12:24:17 +0200

    TDD padding left and fizzbuzz exercises

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 
-function fizzbuzz(number){
-  if( number%3 === 0 && number%5 === 0){
-    return 'fizzbuzz';
-  } if (number%3 === 0 || number.filter(item => item.includes('3'))){
-    return 'fizz';
-  }else if(number%5 === 0){
-    return 'buzz';
-  } else return number;
-};
-module.exports = fizzbuzz;
-// function paddingLeft(string, finalSize, character){
-//   let charactersToAdd = finalSize - string.length;
-//   if (finalSize <= charactersToAdd){
-//     return string;
-//   }else{
-// 	let characters = '';
-//     for (i = 0; i < charactersToAdd; i++){
-// 		characters += character;
-//     }
-// return characters + string;
-//   }
+// function fizzbuzz(number){
+//   if(number%3 === 0 && number%5 === 0){
+//     return 'fizzbuzz';
+//   } if(number%3 === 0 || filterNumber(number) == 'fizz'){
+//     return 'fizz';
+//   } else if(number%5 === 0 || filterNumber(number) == 'buzz'){
+//     return 'buzz';
+//   } else return number;
 // };
-// module.exports = paddingLeft;
+// function filterNumber(number){
+//   numberToString = number.toString();
+//   if (result = numberToString.includes('3')){
+//     return 'fizz';
+//   }else if(result = numberToString.includes('5')){
+//     return 'buzz';
+//   }else return number;
+// }
+// module.exports = fizzbuzz;
 
-// var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-// const result = words.filter(word => word.length > 6);
-
-// var number = 23
-// const result = number.filter(figure => figure.includes()(3))
-// const attendees = this.state.characters.filter(item => item.name.toLowerCase().includes(this.state.search));
-// es
+function paddingLeft(string, finalSize, character){
+  let charactersToAdd = finalSize - string.length;
+  if (finalSize <= charactersToAdd){
+    return string;
+  }else{
+	  let characters = '';
+    for (i = 0; i < charactersToAdd; i++){
+      if (!character){
+        character = ' ';
+      }
+      characters += character;
+    }
+    return characters + string;
+  }
+};
+module.exports = paddingLeft;
--- index.test.js
+++ index.test.js
@@ -1,32 +1,90 @@
-const fizzbuzz = require('./index');
+// //EXERCISE 1
+// const fizzbuzz = require('./index');
 
-describe('Fizzbuzz', function(){
-  test('returns a number', function(){
-   expect(fizzbuzz(7)).toBe(7);
+// describe('Fizzbuzz', function(){
+
+//   test('returns a number', function(){
+//    expect(fizzbuzz(7)).toBe(7);
+//   });
+//   test('returns fizz if number is multiple of 3', function(){
+//     expect(fizzbuzz(3)).toBe('fizz');
+//     expect(fizzbuzz(9)).toBe('fizz');
+//     expect(fizzbuzz(207)).toBe('fizz');
+//    });
+//   test('returns buzz if number is multiple of 5', function(){
+//     expect(fizzbuzz(5)).toBe('buzz');
+//     expect(fizzbuzz(50)).toBe('buzz');
+//     expect(fizzbuzz(2076545)).toBe('buzz');
+//   });
+//   test('returns fizzbuzz if number is multiple of both 3 and 5', function(){
+//     expect(fizzbuzz(15)).toBe('fizzbuzz');
+//     expect(fizzbuzz(90)).toBe('fizzbuzz');
+//     expect(fizzbuzz(1035)).toBe('fizzbuzz');
+//   });
+//   test('returns fizz if number contains 3', function(){
+//     expect(fizzbuzz(143)).toBe('fizz');
+//     expect(fizzbuzz(100000223)).toBe('fizz');
+//   });
+//   test('returns buzz if number contains 5', function(){
+//     expect(fizzbuzz(151)).toBe('buzz');
+//     expect(fizzbuzz(52)).toBe('buzz');
+//   });
+//   test('returns number if not multiple of 3, 5 or doesnt contain 3', function(){
+//     expect(fizzbuzz(14)).toBe(14);
+//     expect(fizzbuzz(1)).toBe(1);
+//   });
+//   test('returns fizz if number contains 3', function(){
+//     var number;
+//     var result;
+//     function returnNumber(number){
+//       num = number.toString();
+//       if (result = num.includes('3')){
+//       return 'fizz';
+//       }else if(result = num.includes('5')){
+//        return 'buzz';
+//       }else return number;
+//     }
+//     expect(returnNumber(3)).toBe('fizz');
+//     expect(returnNumber(6551)).toBe('buzz');
+//     expect(returnNumber(8)).toBe(8);
+//   });
+// });
+
+//EXERCISE 2
+const paddingLeft = require('./index');
+describe('Padding-left', function(){
+  test('returns original stirng', function(){
+    expect(paddingLeft('hello')).toBe('hello');
   });
-  test('returns fizz if number is multiple of 3; 9', function(){
-    expect(fizzbuzz(9)).toBe('fizz');
-   });
-  test('returns buzz if number is multiple of 5; 50', function(){
-    expect(fizzbuzz(50)).toBe('buzz');
+  test('adds 1 character to the left', function(){
+    expect(paddingLeft('hello', 6, 'x')).toBe('xhello');
   });
-  test('returns fizzbuzz if number is multiple of both 3 and 5; 15', function(){
-    expect(fizzbuzz(15)).toBe('fizzbuzz');
+  test('adds 2 characters to the left', function(){
+    expect(paddingLeft('hello', 7, 'x')).toBe('xxhello');
+  });
+  test('adds 3 characters to the left', function(){
+    expect(paddingLeft('hello', 8, 'x')).toBe('xxxhello');
+    expect(paddingLeft('María', 8, 'o')).toBe('oooMaría');
+  });
+  test('adds 3 spaces to the left if not given a character', function(){
+    function paddingLeft(string, finalSize, character){
+      let charactersToAdd = finalSize - string.length;
+      if (finalSize <= charactersToAdd){
+        return string;
+      }else{
+        let characters = '';
+        for (i = 0; i < charactersToAdd; i++){
+          if (!character){
+            character = ' ';
+          }
+          characters += character;
+        }
+        return characters + string;
+      }
+    };
+    expect(paddingLeft('hola', 5)).toBe(' hola');
+    expect(paddingLeft('brujas', 10)).toBe('    brujas');
+
   });
 });
 
-// const paddingLeft = require('./index');
-// describe('Padding-left', function(){
-//   test('adds 1 character to the left', function(){
-//     expect(paddingLeft('hello', 6, 'x')).toBe('xhello');
-//   });
-//   test('adds 2 characters to the left', function(){
-//     expect(paddingLeft('hello', 7, 'x')).toBe('xxhello');
-//   });
-//   test('adds 3 characters to the left', function(){
-//     expect(paddingLeft('hello', 8, 'x')).toBe('xxxhello');
-//   });
-//   test('adds 8 characters to the left', function(){
-//     expect(paddingLeft('María', 8, 'o')).toBe('oooMaría');
-//   });
-// });
