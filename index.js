e5fa883a9d164f6c1397c94d7ed0f87318922170
Teresa <teresamarbancastro@gmail.com>
2018-05-14 13:24:34 +0200

    Session 1 finished

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 
-// function fizzbuzz(number){
-//   if(number%3 === 0 && number%5 === 0){
-//     return 'fizzbuzz';
-//   } if(number%3 === 0 || filterNumber(number) == 'fizz'){
-//     return 'fizz';
-//   } else if(number%5 === 0 || filterNumber(number) == 'buzz'){
-//     return 'buzz';
-//   } else return number;
-// };
-// function filterNumber(number){
-//   numberToString = number.toString();
-//   if (result = numberToString.includes('3')){
-//     return 'fizz';
-//   }else if(result = numberToString.includes('5')){
-//     return 'buzz';
-//   }else return number;
-// }
-// module.exports = fizzbuzz;
-
-
-function paddingLeft(string, finalSize, character){
-  let charactersToAdd = finalSize - string.length;
-  if (finalSize <= charactersToAdd){
-    return string;
-  }else{
-	  let characters = '';
-    for (i = 0; i < charactersToAdd; i++){
-      if (!character){
-        character = ' ';
-      }
-      characters += character;
-    }
-    return characters + string;
-  }
+function fizzbuzz(number){
+  if(number%3 === 0 && number%5 === 0 || filterNumber(number) == 'fizzbuzz'){
+    return 'fizzbuzz';
+  } if(number%3 === 0 || filterNumber(number) == 'fizz'){
+    return 'fizz';
+  } else if(number%5 === 0 || filterNumber(number) == 'buzz'){
+    return 'buzz';
+  } else return number;
 };
-module.exports = paddingLeft;
+function filterNumber(number){
+  numberToString = number.toString();
+  if((result = numberToString.includes('3')) && (result = numberToString.includes('5'))){
+    return 'fizzbuzz';
+  }else if (result = numberToString.includes('3')){
+    return 'fizz';
+  }else if(result = numberToString.includes('5')){
+    return 'buzz';
+  }else return number;
+}
+module.exports = fizzbuzz;
+
+
+// function paddingLeft(string, finalSize, character){
+//   let charactersToAdd = finalSize - string.length;
+//   if (finalSize <= charactersToAdd){
+//     return string;
+//   }else{
+// 	  let characters = '';
+//     for (i = 0; i < charactersToAdd; i++){
+//       if (!character){
+//         character = ' ';
+//       }
+//       characters += character;
+//     }
+//     return characters + string;
+//   }
+// };
+// module.exports = paddingLeft;
